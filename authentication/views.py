8f3549f9ddf6eb3f337dcf3bb74a33a9154cba48
Mithilesh Kohale <beingtmk@gmail.com>
2019-04-16 15:32:13 +0530

    fixes

--- authentication/views.py
+++ authentication/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect, reverse
+from django.shortcuts import HttpResponse, reverse, redirect
 
-from django.shortcuts import render
-from django.urls import reverse
+# The Sign In Url to redirect to Outlook Login
 from authentication.authhelper import get_signin_url
-import time
 
-# Add import statement to include new function
-from authentication.outlookservice import get_me
-from authentication.authhelper import get_signin_url, get_token_from_code, get_access_token
+# Outlook gives us authorization code, we ask for a token from it
+from authentication.authhelper import get_token_from_code
+
+# The helper to get data from outlook like roll number, email, name
 from authentication.outlookservice import get_me
 
 def home(request):
-    redirect_uri = request.build_absolute_uri(
-        reverse('authentication:gettoken'))
-    sign_in_url = get_signin_url(redirect_uri)
-    return HttpResponse('<a href="' + sign_in_url + '">Click here to sign in and test outlook OAuth2</a>')
+
+  # Redirecting to gettoken view after authenticating
+  redirect_uri = request.build_absolute_uri(
+      reverse('authentication:gettoken'))
+
+  # Building the sig in url
+  sign_in_url = get_signin_url(redirect_uri)
+
+  return HttpResponse('<a href="' + sign_in_url + '">Click here to sign in and test outlook OAuth2</a>')
 
 
 # Add import statement to include new function
@@ -26,6 +30,8 @@ def gettoken(request):
   redirect_url = None
   ################################
 
+
+  # get Token from code
   auth_code = request.GET['code']
   redirect_uri = request.build_absolute_uri(reverse('authentication:gettoken'))
   token = get_token_from_code(auth_code, redirect_uri)
@@ -37,7 +43,11 @@ def gettoken(request):
   # redirect_url = request.session.get('redirect_url', None)
   
   if redirect_url is None:
+
+    #####################
+    # Get user from token
     user = get_me(access_token)
+
     return HttpResponse("Token: %s<br>Name: %s<br>Roll Number: %s<br> Mail: %s" % (access_token, user['displayName'], user['surname'], user['mail']))
   else: 
     return redirect(redirect_url)
